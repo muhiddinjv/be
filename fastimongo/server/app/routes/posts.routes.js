4d934136cf90ee19c420629868c0e5ceac624a48
Мухиддин Х. Жалилходжаев <jalilxodjayev.m@ad.loc>
2022-12-02 20:04:54 +0500

    feat: add getPostsOpts

--- fastimongo/server/app/routes/posts.routes.js
+++ fastimongo/server/app/routes/posts.routes.js
@@ -1,25 +1,33 @@
-const {
-  getPosts,
-  addPost,
-  getPost,
-} = require("../actions/posts.controller");
-
-async function routes(fastify, options) {
-  fastify.get("/posts", getPosts);
-  fastify.post("/posts", addPost);
-  fastify.get("/posts/:id", getPost);
-}
-
 const allposts = [
   { id: 1, title: 'Post One', body: 'This is post one' },
   { id: 2, title: 'Post Two', body: 'This is post two' },
   { id: 3, title: 'Post Three', body: 'This is post three' },
-]
+];
+
+const getPostsOpts = {
+  schema:{
+    response:{
+      200: {
+        type: 'array',
+        items: {
+          type: 'object',
+          properties: {
+            id: {type: 'number'},
+            title: {type: 'string'},
+            body: {type:'string'},
+          }
+        }
+      }
+    }
+  },
+  handler:(request, reply) => {
+    reply.send(allposts);
+  }
+}
 
 const postRoutes = (fastify, options, done) => {
-    fastify.get('/', (request, reply) => {
-        reply.send(allposts);
-    })
+    fastify.get('/', getPostsOpts)
     done();
 }
-  module.exports = postRoutes;
+
+module.exports = postRoutes;
