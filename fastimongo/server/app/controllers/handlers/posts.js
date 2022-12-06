c1ebd985f32070158f6781d4025f3face64b77e9
Мухиддин Х. Жалилходжаев <jalilxodjayev.m@ad.loc>
2022-12-06 13:43:20 +0500

    feat: delete post

--- fastimongo/server/app/controllers/handlers/posts.js
+++ fastimongo/server/app/controllers/handlers/posts.js
@@ -29,17 +29,16 @@ const addPostHandler = (req, reply) => {
 
 const deletePostHandler = (req, reply) => {
   const { id } = req.params;
+  
+  const foundPost = posts.find(post => post.id === id);
 
-  let post = posts.filter((post) => {
-    return post.id !== id;
-  });
+  if(!foundPost){
+    return reply.status(404).send(new Error('Post not found'));
+  }
+  
+  posts = posts.filter(post => post.id !== id)
 
-  posts.find(post => post.id !== id && reply.status(404).send(new Error("Post doesn't exist"))
-  );
-
-  posts = post;
-
-  reply.send('Post deleted');
+  return reply.send('Post deleted');
 };
 
 module.exports = { getPostsHandler, getPostHandler, addPostHandler, deletePostHandler };
